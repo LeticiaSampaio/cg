041ce48c1db4d901e864faa95ec1316f875c5fad
Leticia Sampaio <leticia.sampaio@prodigious.com>
2020-07-27 00:29:03 -0300

    Menu dropDown para ficar de acordo com especificação do trabalho.

--- Trabalho 1/trabalho1.js
+++ Trabalho 1/trabalho1.js
@@ -1,5 +1,32 @@
-const canvas = document.querySelector('#draw');
+const canvasDiv = document.getElementById('canvas');
+const canvasFieldset = document.getElementById('canvasFS');
+const canvas = document.getElementById('draw');
+const dropDown = document.getElementById('selectModel');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth - 300;
-canvas.height = window.innerHeight - 30;
+canvasDiv.width = window.innerWidth - 300;
+canvasDiv.height = window.innerHeight - 30;
+
+canvas.width = window.innerWidth - 330;
+canvas.height = window.innerHeight - 70;
+
+dropDown.addEventListener("change", (event) => {
+  var value = event.target.value;
+  var model = document.getElementById('model');
+  var element = document.getElementById(value);
+  var oldElement = document.querySelector('.shown');
+  
+  if(element.id != "addModel"){
+    model.classList.remove("hidden");
+    model.classList.add("shown");
+  }else{
+    model.classList.remove("shown");
+    model.classList.add("hidden");
+  }
+  element.classList.remove("hidden");
+  element.classList.add("shown");
+  if(oldElement != null){
+    oldElement.classList.remove("shown");
+    oldElement.classList.add("hidden"); 
+  }
+});
